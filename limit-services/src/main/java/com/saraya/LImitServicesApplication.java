b9c84e7858cb073fb25c98ae4c6cc577e7fe8e65
ousmaneG <gassamaousmane@gmail.com>
2022-01-18 20:47:33 +0000

    add properties

---
+++ limit-services/src/main/java/com/saraya/LImitServicesApplication.java
@@ -0,0 +1,13 @@
+package com.saraya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LImitServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LImitServicesApplication.class, args);
+	}
+
+}
